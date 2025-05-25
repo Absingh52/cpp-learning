f869896b2da50734cc7de318eb334dd83161bdf0
Absingh52 <abhisheksingh26460@gmail.com>
2025-05-25 23:00:46 +0530

    feat: Added C++ character case check and pattern debug code

---
+++ intro to c++/exercise1/debug the code.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Question 1 print lowercase and uppercase if it is upper print 1 and if lower print 0
+    // char c;
+    // cin>>c;
+    // if(c>='a' && c<= 'z'){
+    //     cout<<0;
+    // }
+	// else if(c>='A' && c<= 'Z'){
+    //     cout<<1;
+    // }
+    // else{
+    //     cout<<-1;
+    // }
+
+    //Question2 It is trying to print the given pattern.
+    /*
+        Pattern
+        1
+        23
+        345
+        4567
+    */
+    //  int n=5;
+    // int i=1;
+    // cin>>n;
+    // while(i<n){
+    //     int j = i,count = 0;
+    //     while(count<i){
+    //         cout<<j;
+    //         j = j + 1;
+    //         count = count + 1;
+    //     }
+    //     cout<<"\n";
+    //     i = i + 1;
+    // }
+   return 0;
+}
