6621dde5cc54292b76ce6ddaf75580927d4b588e
Absingh52 <abhisheksingh26460@gmail.com>
2025-06-01 17:18:50 +0530

    New updates

--- intro to c++/day3/main.cpp
+++ intro to c++/day3/main.cpp
@@ -26,14 +26,56 @@ int main()
         }
         cout<<endl;
     }    
-    
-    /*
+     
+
+    /* 
+    question 3 inverted triangle
     */
      for(int i=1;i<=n;i++){
         for(int j=5;j>=i;j--){
             cout<<"*";
         }
         cout<<endl;
+    } 
+    
+    /* 
+    question 4 right-aligned triangle
+    */
+
+     for(int i=1;i<=n;i++){
+       int k=i;
+          while (k<=n-1)
+         {
+            cout<<" ";
+            k++;
+            
+         }
+         
+        for(int j=1;j<=i;j++){
+          
+            cout<<"*";
+        }
+        cout<<endl;
+    }    
+
+    /*
+    question 5 number triangle
+    */
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
     }
+    
+    
+    /*Question 6 reverse number triangle*/   
+     for(int i=0;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    } 
+    
     return 0;
 }
