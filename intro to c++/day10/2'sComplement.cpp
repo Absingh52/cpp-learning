3a37b52913d685f172ef3113899c9c5fc09e8a6b
Abhishek Singh <abhisheksingh26460@gmail.com>
2025-06-24 00:07:29 +0530

    Update 2'sComplement.cpp

--- intro to c++/day10/2'sComplement.cpp
+++ intro to c++/day10/2'sComplement.cpp
@@ -2,9 +2,10 @@
 // find 2's complement 
 using namespace std;
 int main()
-{       int num;
-    cin>> num;
+{      int num;
+     cout<<"enter number:";
+        cin>> num;
       int result =~num+1;
-        cout<< result;
+        cout<< "the 2's Complement is:"<<result;
 } // namespace std;
 
